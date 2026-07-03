0f6344396ae61c32fa15220d91177da0d95a36ef
Sultan <211139503+Shuvo1910@users.noreply.github.com>
2026-07-04 03:52:54 +0600

    Add files via upload

---
+++ hello_print.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+// h -> header file
+// stdio:
+// std -> standard , i -> input, o -> output
+
+int main() {
+
+// int -> integer 
+// main -> method (starting point to execute code)
+
+    printf("Welcome to C Programming !"); 
+
+// printf => print something as output.
+
+    return 0;
+}
